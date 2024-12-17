09f19921295d4a84fd5ab4fa57c08b069b819ede
mohammedh <mohammedh@zeta.tech>
2024-12-17 18:34:28 +0530

    added resection feature on job list page to improve user experience

--- src/components/CompanyList.tsx
+++ src/components/CompanyList.tsx
@@ -1,8 +1,9 @@
 import companyNames from "../json/company.json";
 interface CompanyListProps {
   is_selected: (selected: boolean) => void;
+  selected_option?: string;
 }
-const CompanyList: React.FC<CompanyListProps> = ({is_selected}) => {
+const CompanyList: React.FC<CompanyListProps> = ({is_selected, selected_option}) => {
   const getCompanyNames = () => {
     return companyNames.map((item) => (
       <option key={item.name} value={item.name}>
@@ -15,14 +16,18 @@ const CompanyList: React.FC<CompanyListProps> = ({is_selected}) => {
     is_selected(true);
   };
   return (
+   <>
     <select
       name="options"
       className="custom-select"
+      value={selected_option}
       onChange={handleSelectChange}
     >
       <option value="">Select Company</option>
       {getCompanyNames()}
     </select>
+     {selected_option &&  <span className="custom-select-tooltip">{selected_option}</span> }
+   </>
   );
 };
 
